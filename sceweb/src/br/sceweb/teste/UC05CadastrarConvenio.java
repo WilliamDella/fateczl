cab72d9dfad36f307cebd9428060fbcdeb645adf
WilliamDella <williamdellaripa@hotmail.com>
2016-05-13 15:12:15 -0300

    Analisando a cobertura

--- sceweb/src/br/sceweb/teste/UC05CadastrarConvenio.java
+++ sceweb/src/br/sceweb/teste/UC05CadastrarConvenio.java
@@ -2,7 +2,6 @@ package br.sceweb.teste;
 
 import static org.junit.Assert.*;
 
-import org.joda.time.DateTime;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -32,29 +31,38 @@ public class UC05CadastrarConvenio {
 		convenioDAO = new ConvenioDAO();
 		convenio = new Convenio("81965361000174","03/05/2016", "20/05/2016");
 	}
+	
 	@Test
 	public void CT01UC05CadastrarConvenio_com_sucesso() {
 		assertEquals(1,convenioDAO.adiciona(convenio));
 	}
 	
-	@Test(expected=IllegalArgumentException.class)
-	public void CT06UC05A3CadastrarConvenio_cnpj_invalido() {
-		cnpj = "11111";
-		convenio.setCNPJ(cnpj);
+	@Test
+	public void CT06UC05A3Cadastrar_convenio_cnpj_nao_cadastrado(){
+		Convenio novoConvenio = new Convenio("95754698523149", "03/05/2016", "20/05/2016");
+		assertEquals(0,convenioDAO.adiciona(novoConvenio));
 	}
+		
+	
+//	@Test(expected=IllegalArgumentException.class)
+//	public void CT05UC05A3CadastrarConvenio_cnpj_invalido() {
+//		cnpj = "11111";
+//		convenio.setCNPJ(cnpj);
+//	}
+	
 	@Test
 	public void CT03UC05A2Cadastrar_convenio_dti_invalida(){
 		assertFalse(convenio.validaData("42/05/2016"));
 	}
-	@Test
-	public void CT02UC05A1Cadastrar_convenio_ja_cadastrado(){
-		DateTime di = new DateTime(2016, 4, 5, 0, 0);
-		DateTime df = new DateTime(2016, 5, 1, 0, 0);
-		assertEquals (4, convenio.convenioJaCadastrado(df,di));
-	}
+	
+//	@Test
+//	public void CT02UC05A1Cadastrar_convenio_ja_cadastrado(){
+//		DateTime di = new DateTime(2016, 4, 5, 0, 0);
+//		DateTime df = new DateTime(2016, 5, 1, 0, 0);
+//		assertEquals (4, convenio.convenioJaCadastrado(df,di));
+//	}
 
-	@
-	AfterClass
+	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		convenioDAO.exclui("81965361000174");
 		empresaDAO.exclui("81965361000174");
